837c17f67a452f1aa3e93e25c5cc3fcfe6880e8d
Felipe Kurkowski <felipekurkowski@gmail.com>
2015-05-27 21:44:57 -0300

    Switched to specs2 unit testing syntax.

--- src/test/scala/com/fkurkowski/trophy/ThophyServletSpec.scala
+++ src/test/scala/com/fkurkowski/trophy/ThophyServletSpec.scala
@@ -3,14 +3,14 @@ package com.fkurkowski.trophy
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class ThophyServletSpec extends ScalatraSpec { def is =
-  "GET / on ThophyServlet"                     ^
-    "should return status 200"                  ! root200^
-                                                end
-
+class ThophyServletSpec extends MutableScalatraSpec {
   addServlet(classOf[TrophyServlet], "/*")
 
-  def root200 = get("/") {
-    status must_== 200
+  "GET / on ThophyServlet" should {
+    "return status 200" in {
+      get("/") {
+        status === 200
+      }
+    }
   }
 }
