7c827e39fb54f0eb6aa28fe776be25576768ec36
Felipe Kurkowski <felipekurkowski@gmail.com>
2015-05-31 23:46:27 -0300

    Querying (roughly) from DB now.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,25 @@
 import com.fkurkowski.trophy.controller.TrophyServlet
+import com.fkurkowski.trophy.domain.Tables
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import com.typesafe.scalalogging.LazyLogging
 import org.scalatra._
 import javax.servlet.ServletContext
+import slick.driver.H2Driver.api.Database
 
-class ScalatraBootstrap extends LifeCycle {
+class ScalatraBootstrap extends LifeCycle with LazyLogging {
+
+  val cpds = new ComboPooledDataSource
+  logger.info("Created connection pool with c3p0")
+
   override def init(context: ServletContext) {
-    context.mount(new TrophyServlet, "/*")
+    val db = Database.forDataSource(cpds);
+    db.run(Tables.setup)
+    context.mount(new TrophyServlet(db), "/*")
+  }
+
+  override def destroy(context: ServletContext) {
+    super.destroy(context)
+    logger.info("Closing connection pool")
+    cpds.close()
   }
 }
--- src/main/scala/com/fkurkowski/trophy/controller/TrophyServlet.scala
+++ src/main/scala/com/fkurkowski/trophy/controller/TrophyServlet.scala
@@ -1,15 +1,30 @@
 package com.fkurkowski.trophy.controller
 
-import com.fkurkowski.trophy.domain.VideoStore
+import com.fkurkowski.trophy.domain.Tables.videos
+import org.scalatra.{AsyncResult, FutureSupport}
+import slick.driver.H2Driver.api._
+import scala.concurrent.ExecutionContext
+import scala.util.Random
 
-class TrophyServlet extends TrophyRoomStack {
+
+class TrophyServlet(db: Database) extends TrophyRoomStack with FutureSupport {
+
+  protected implicit def executor: ExecutionContext = ExecutionContext.Implicits.global
 
   before() {
     contentType = "text/html"
   }
 
   get("/") {
-    mustache("index", "video" -> VideoStore.random)
+    new AsyncResult() {
+      /**
+       * TODO: optimize this
+       */
+      val is = db.run(videos.result) map { v =>
+        val idx = Random.nextInt(v.size)
+        mustache("index", "video" -> v(idx))
+      }
+    }
   }
 
   get("/videos") {
